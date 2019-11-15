a1fe1f25dae1efefce977680097a6037646ca537
Skin <rizqinnazhif@gmail.com>
2019-11-15 09:57:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,3 +13,10 @@ function logShout(string) {
 function logWhisper(string) {
   console.log(string.toLowerCase());
 }
+
+function sayHiToGrandma(string) {
+  if(string.toUpperCase() === false) {
+    return "I can't hear you!";
+  }
+  return "YES INDEED!";
+}
